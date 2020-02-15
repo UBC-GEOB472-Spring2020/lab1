0f7737e5afda61202b94bf701f7169ff6cbc2e3c
whyname <lanyu82082182@gmail.com>
2020-02-14 18:11:36 -0800

    Update mapbox-gl.js

--- mapbox-gl.js
+++ mapbox-gl.js
@@ -1,9 +1,1 @@
 
-
-    mapboxgl.accessToken = 'pk.eyJ1IjoiaW1tbW9yIiwiYSI6ImNrNmxhYTZvcDA5cTgzZHBkOThxMTl3N2IifQ.St9SZGWOIL04YeKFSuzYBw';
-      
-      var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/immmor/ck6mxc2qh1k891iplwlt3wwhk' // replace this with your style URL
-});
-      
